72ab22dfc99c003a24ad1b641c421d3befac6d97
Martin Pölzl (PSE) <pom82@gmx.at>
2021-04-04 12:18:54 +0200

    edit readme

--- src/at/campus02/nowa/App.java
+++ src/at/campus02/nowa/App.java
@@ -11,11 +11,12 @@ public class App {
     private boolean exit = false;
     private int figureNr;
     private Figure figure;
+    private int grosse;
 
     // Konstruktor
     // input wird verwendet um Daten vom Benutzer einzulesen (verwendet scanner)
     // output wird verwendet um Text auf der Konsole auszugeben (verwendet sysout)
-    public App(Scanner input, PrintStream output){
+    public App(Scanner input, PrintStream output) {
         this.input = input;
         this.output = output;
     }
@@ -25,7 +26,7 @@ public class App {
         initialize();
         printState();
 
-        while(!exit) {
+        while (!exit) {
             readUserInput();
             updateState();
             printState();
@@ -44,32 +45,67 @@ public class App {
     private void updateState() {
         //TODO: Benutzereingaben verarbeiten
 
-        switch (figureNr){
-            case 1:
-                figure = new FigureH();
+        switch (figureNr) {
+            case 0:
+                exit = true;
                 break;
-            default: break;
+            case 1:
+                figure = new FigureH(grosse) ;
+                break;
+            case 2:
+                figure = new FigureL(grosse);
+                break;
+            case 3:
+                figure = new FigureO(grosse);
+                break;
+            case 4:
+                figure = new FigureO2(grosse);
+
+                break;
+            case 5:
+                figure = new FigureI(grosse);
+                break;
+            case 6:
+                figure = new Minus(grosse);
+
+                break;
+            default:
+                break;
         }
+
+
+
     }
 
     private void printState() {
         //TODO: Ausgabe des aktuellen Zustands
-        if (figure != null){
-            output.println(figure);
+
+        if (exit == true) {
+            System.out.println("Game End");
+        } else if (figure != null) {
+            output.println(figure );
+
         }
     }
 
     private void inputFigure() {
         // Hier sehen Sie ein Pattern für Benutzereingaben
         // Solange kein gültiger Wert eingegeben wurde, wird die Eingabe wiederholt
+
         do {
             output.println("Welche Grafik möchten Sie anzeigen (1-6)");
             figureNr = input.nextInt();
-            if (figureNr < 1 || figureNr > 6) {
+            System.out.println("Welche Größe möchten sie wählen (1-3)");
+            grosse=input.nextInt();
+            if (figureNr < 0 || figureNr > 6) {
                 output.println("Dies ist keine gültige Grafik!");
-            } else {
-                break;
             }
+            if (grosse < 1 || grosse > 10) {
+                output.println("Dies ist keine gültige Größe");
+            }
+
+                break;
+
         } while (true);
     }
 }
--- src/at/campus02/nowa/figures/Figure.java
+++ src/at/campus02/nowa/figures/Figure.java
@@ -1,18 +1,84 @@
 package at.campus02.nowa.figures;
 
+import java.util.Arrays;
+
 public abstract class Figure {
     protected char[][] symbol = new char[3][3];
+    protected int size;
+
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                sb.append(symbol[col][row]);
-            }
-            sb.append('\n');
-        }
 
-        return sb.toString();
+        StringBuilder sb = new StringBuilder();
+//        for (int x = 0; x < size; x++) {
+        for (int row = 0; row < 3; row++) {
+
+            for (int p = 0; p < size; p++) {
+                for (int col = 0; col < 3; col++) {
+                    for (int i = 0; i < size; i++) {
+                        sb.append(symbol[col][row]);
+
+                    }
+                }
+                sb.append('\n');
+            }
+
+//                for (int col = 0; col < 3; col++) {
+//
+//                    sb.append(symbol[col][row]);
+//
+//                }
+//
+//                } sb.append('\n');
+
+
+        }return sb.toString();
     }
-}
+    }
+
+
+
+
+
+//            } else if (size == 2) {
+//                for (int col = 0; col < 3; col++) {
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//
+//                }
+//                sb.append('\n');
+//                for (int col = 0; col < 3; col++) {
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//
+//                }
+//                sb.append('\n');
+//            }
+//            else if (size==3){
+//                for (int col = 0; col < 3; col++) {
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//
+//                }
+//                sb.append('\n');
+//                for (int col = 0; col < 3; col++) {
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//
+//                }
+//                sb.append('\n');
+//                for (int col = 0; col < 3; col++) {
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//                    sb.append(symbol[col][row]);
+//
+//                }
+//                sb.append('\n');
+//
+//            }
+
+
+
